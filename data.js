e965b480a42cd7c4f2fe483ed4e53feda101c5ca
Vicus Brits <vicusbritsmail@gmail.com>
2024-10-18 14:25:49 +0200

    Basic HTML Structure complete

---
+++ data.js
@@ -0,0 +1,81 @@
+let userType = null; // client or technician
+var technicians = [
+  {
+    id: 1,
+    username: "Any Technician",
+    area: "",
+    email: "serviceteam@hestico.co.za",
+    password: "123",
+    mobile: "0829040718",
+  },
+  {
+    id: 2,
+    username: "Victor",
+    area: "Durban",
+    email: "victor@hestico.co.za",
+    password: "123",
+  },
+  {
+    id: 3,
+    username: "Lourens",
+    area: "Johannesburg",
+    email: "lourens@hestico.co.za",
+    password: "123",
+    mobile: "0829040718",
+  },
+  {
+    id: 4,
+    username: "Gus",
+    area: "Cape Town",
+    email: "gus@hestico.co.za",
+    password: "123",
+    mobile: "0829040718",
+  },
+];
+
+var clients = [
+  {
+    id: 1,
+    username: "PlastiPack",
+    email: "tony@plasticpack.co.za",
+    area: "Johannesburg",
+    password: "123",
+  },
+  {
+    id: 2,
+    username: "TurtlePlas",
+    email: "john@TurtlePlas.co.za",
+    area: "Durban",
+    password: "123",
+  },
+];
+
+var equipment = [
+  "Arburg",
+  "Engel",
+  "Haitian",
+  "Husky",
+  "Krauss Maffei",
+  "Negri Bossi",
+  "Netstal",
+  "Sumitomo",
+  "Toshiba",
+  "Van Dorn",
+];
+
+var serviceTypes = [
+  "Service",
+  "Repair",
+  "Installation",
+  "Training",
+  "Consultation",
+  "Other",
+];
+
+var wostatus = [
+  "Service Call",
+  "Service Booked",
+  "Works Order Request",
+  "Works Order Approved",
+  "Invoice",
+];
