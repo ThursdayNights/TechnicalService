fb5ca40094300fdd3f64ea36ec59a35295eeee2c
Vicus Brits <vicusbritsmail@gmail.com>
2024-10-19 12:12:31 +0200

    function handleNavigation(href) implemented

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let globalState = {
   username: "",
   password: "",
   type: "",
+  currentstate: "login",
 };
 
 function handleLogin(event) {
@@ -28,8 +29,31 @@ function handleLogin(event) {
   alert(globalState.type);
 }
 
-function handleButtonNavigation(href) {
-  // navigate to href specified
-  // e.g. "approve-works-order" navigates to href="#approve-works-order"
+function goToSection(href) {
   window.location.href = `#${href}`;
+  globalState.currentstate = href;
+}
+
+function handleNavigation(href) {
+  // Client handler
+  if (globalState.type === "client") {
+    if (href === "login") {
+      goToSection("client-landing");
+      return;
+    } else {
+      goToSection("login");
+    }
+  }
+  // Service handler
+  if (globalState.type === "service") {
+    if (href === "login") {
+      goToSection("view-requests");
+      return;
+    } else {
+      goToSection("login");
+    }
+
+    // Default handler
+    goToSection("login");
+  }
 }
